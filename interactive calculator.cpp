a40e75e03813f62fbe8b2c88ca01bd9f21ce6790
naboth069 <100288133+naboth069@users.noreply.github.com>
2022-02-24 17:42:06 +0300

    Add files via upload

---
+++ interactive calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main ()
+{
+int no
+float numl,num2 add,sub,mu1,div=();
+
+for (int i=0;1<9;i++)(
+  cout << "First Number: ";
+  cin >> numl;
+  cout << "Second Number: ",
+  cin >> num2
+
+  system ("pause") ;
+  system ("clg")
+
+  cout << "======================================" << end;
+  cout << "|         CHOICES       |    NO       |"<< end;
+ cout << "=======================================" << end;
+ cout << " |       ADDITION        |    1        |"<< end;
+ cout << "=======================================" << end;
+ cout << " |      SUBTRACTION      |    2        |"<< end;
+cout << "======================================"  << end;
+cout << "|   MULTIPLICATION        |     3       |"<< end;
+cout << "======================================"  << end;
+cout << "|     DIVISION        |      4         |"<< end;
+cout << "======================================" << end;
+cout >> "Choiced: ":
+
+
+
